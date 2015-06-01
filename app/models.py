d3308eac1b2cf2f71003c885bba43f9692256aad
Hyun Jun, An <dltn123@gmail.com>
2015-06-01 22:12:45 +0900

    Update 2015-06-01

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from app import db, bcrypt
+from hashlib import md5
 
 
 class User(db.Model):
@@ -10,6 +11,8 @@ class User(db.Model):
     pw_hash = db.Column(db.String(128))
     email = db.Column(db.String(128), index=True, unique=True)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime)
 
     def is_authenticated(self):
         return True
@@ -20,11 +23,15 @@ class User(db.Model):
     def is_anonymous(self):
         return False
 
+    def avatar(self, size):
+        return 'http://www.gravatar.com/avatar/%s?d=mm&s=%d' % \
+            (md5(self.email.encode('utf-8')).hexdigest(), size)
+
     def get_id(self):
         try:
-            return unicode(self.id)		# python 2
+            return unicode(self.id)     # python 2
         except NameError:
-            return str(self.id)			# python 3
+            return str(self.id)         # python 3
 
     def check_password(self, password):
         return bcrypt.check_password_hash(self.pw_hash, password)
