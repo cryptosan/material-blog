c12173595de1d9338f1c03960902eaf62f3eb400
Hyun Jun, An <dltn123@gmail.com>
2015-06-08 00:29:40 +0900

    Add custom paths of templates, static.

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,19 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-import os
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
 from flask.ext.bcrypt import Bcrypt
-from config import basedir
+from flask.ext.register import RegisterManager
 
 
 app = Flask(__name__)
 app.config.from_object('config')
+# Set up paths of templates, and static folder.
+app.static_folder = app.config['STATIC_PATH']
+app.template_folder = app.config['TEMPLATES_PATH']
+
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
@@ -18,6 +21,22 @@ bcrypt = Bcrypt(app)
 lm = LoginManager()
 lm.init_app(app)
 lm.login_view = 'login'
+
+signup = RegisterManager(app)
+signup.save_redirect_view('index')
+
+
+if not app.debug:
+    import logging
+    from logging.handlers import RotatingFileHandler
+    file_handler = RotatingFileHandler('tmp/mdblog.log', 'a',
+                                       1 * 1024 * 1024, 10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    app.logger.setLevel(logging.INFO)
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.info('material blog startup')
 
 
 from app import views, models
