6ef9f9e2486c435a76a26289aed6fd34a16f2253
Hyun Jun, An <dltn123@gmail.com>
2015-06-08 00:48:39 +0900

    Add PostForm - Add PostForm - Update EditForm structures, and validate - Update validate function of RegisterForm - Update validate function of LoginForm

--- app/forms.py
+++ app/forms.py
@@ -3,9 +3,9 @@ from __future__ import unicode_literals
 
 from flask.ext.wtf import Form
 from wtforms import PasswordField, StringField, SubmitField, BooleanField, \
-    TextAreaField, ValidationError
+    TextAreaField
 from wtforms.validators import Email, DataRequired, Length, EqualTo, \
-    Required, Regexp
+    Regexp
 from .models import User
 
 
@@ -15,9 +15,21 @@ class LoginForm(Form):
         Length(1, 64),
         Email('It\'s not an Email!')
     ])
-    password = PasswordField('Password', [DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('remember_me', default=False)
     submit = SubmitField('Submit')
+
+    def validate(self):
+        if not Form.validate(self):
+            return False
+        user = User.query.filter_by(email=self.email.data).first()
+        if user is None:
+            self.email.errors.append('Incorrect email!')
+            return False
+        if not user.check_password(self.password.data):
+            self.password.errors.append('Invalid password!')
+            return False
+        return True
 
 
 class RegisterForm(Form):
@@ -40,15 +52,49 @@ class RegisterForm(Form):
     confirm = PasswordField('Repeat Password')
     submit = SubmitField('Register')
 
-    def validate_email(self, field):
-        if User.query.filter_by(email=field.data).first():
-            raise ValidationError('Email already registered.')
-
-    def validate_nickname(self, field):
-        if User.query.filter_by(nickname=field.data).first():
-            raise ValidationError('Nickname already registered.')
+    def validate(self):
+        if not Form.validate(self):
+            return False
+        email = User.query.filter_by(email=self.email.data).first()
+        if email is not None:
+            self.email.errors.append('This email is already in use.')
+            return False
+        nickname = User.query.filter_by(nickname=self.nickname.data).first()
+        if nickname is not None:
+            self.nickname.errors.append('This nickname is already in use.')
+            return False
+        return True
 
 
 class EditForm(Form):
-    nickname = StringField('nickname', validators=[DataRequired()])
-    about_me = TextAreaField('about_me', validators=[Length(min=0, max=140)])
+    nickname = StringField('Nickname', validators=[
+        DataRequired(),
+        Regexp(
+            '^[A-Za-z][A-Za-z0-9_.]*$', 0,
+            'Nicknames must have only letters, numbers, dots or underscores'
+        ),
+        Length(min=4, max=12)
+    ])
+    about_me = TextAreaField('about_me', validators=[
+        Length(min=0, max=140)
+    ])
+
+    def __init__(self, original_nickname, *args, **kwargs):
+        Form.__init__(self, *args, **kwargs)
+        self.original_nickname = original_nickname
+
+    def validate(self):
+        if not Form.validate(self):
+            return False
+        if self.nickname.data == self.original_nickname:
+            return True
+        user = User.query.filter_by(nickname=self.nickname.data).first()
+        if user is not None:
+            self.nickname.errors.append(
+                'This nickname is already in use, Please choose another one.')
+            return False
+        return True
+
+
+class PostForm(Form):
+    post = StringField('post', validators=[DataRequired()])
