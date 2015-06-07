7d52117c7793ecca3d7f9eb65a8500ceab318666
Hyun Jun, An <dltn123@gmail.com>
2015-06-08 00:50:54 +0900

    Add followers table. - Add followers table. - Add followerd field into User - Add follow/unfollow/is_following/followed_posts funcs into User

--- app/models.py
+++ app/models.py
@@ -5,6 +5,13 @@ from app import db, bcrypt
 from hashlib import md5
 
 
+followers = db.Table(
+    'followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nickname = db.Column(db.String(64), index=True, unique=True)
@@ -13,6 +20,14 @@ class User(db.Model):
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime)
+    followed = db.relationship(
+        'User',
+        secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'),
+        lazy='dynamic'
+    )
 
     def is_authenticated(self):
         return True
@@ -26,6 +41,26 @@ class User(db.Model):
     def avatar(self, size):
         return 'http://www.gravatar.com/avatar/%s?d=mm&s=%d' % \
             (md5(self.email.encode('utf-8')).hexdigest(), size)
+
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+            return self
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+            return self
+
+    def is_following(self, user):
+        return self.followed.filter(followers.c.followed_id == user.id). \
+            count() > 0
+
+    def followed_posts(self):
+        return Post.query.join(followers, (
+            followers.c.followed_id == Post.user_id)). \
+            filter(followers.c.follower_id == self.id). \
+            order_by(Post.timestamp.desc())
 
     def get_id(self):
         try:
